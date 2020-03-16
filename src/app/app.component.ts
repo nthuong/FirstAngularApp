0e2287820353d147898ff58a39d7e35a2752235b
Huong Nguyen <nthuong1@tma.com.vn>
2020-03-16 18:23:19 +0700

    16-mar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  loadedFeature = 'recipe';
-  onNavigate(feature: string) {
-     console.log(feature);
-    this.loadedFeature = feature;
-  }
-}
+export class AppComponent {}
